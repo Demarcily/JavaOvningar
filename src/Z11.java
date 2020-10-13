621fb44e0ad40332bcc9b3c7fd63df8978d0e758
Demarcily <60388907+Demarcily@users.noreply.github.com>
2020-10-13 10:08:24 +0200

    Importing files from folder

---
+++ src/Z11.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Z11 {
+	public static void main (String[] arg) {
+		JOptionPane.showMessageDialog(null, "Hello ");
+		
+	}
+}
---
+++ src/Z33.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class Z33 {
+	public static void main (String[] arg) {
+		JOptionPane.showMessageDialog(null, "hello");
+
+	}
+}
